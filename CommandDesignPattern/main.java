1449aefcc20e70a797bcf5795e2951029a2ff784
Aman <amangoel9873572693@gmail.com>
2025-12-21 21:54:29 +0530

    21st December 2025: Command Design Pattern.

--- CommandDesignPattern/main.java
+++ CommandDesignPattern/main.java
@@ -1,5 +1,12 @@
 package CommandDesignPattern;
 
 public class main {
-    
+    public static void main(String[] args) {
+        LightReceiver light = new LightReceiver("Off");
+
+        CommandInterface lighOnCommand = new LightOnCommand(light);
+
+        RemoteControlInvoker remoteControl = new RemoteControlInvoker(lighOnCommand);
+        remoteControl.buttonPressed();
+    }
 }
